306b4cbd1ac804ca0dd3131b159a14cd324d4e16
Hugo Gruson <hugo.gruson@protonmail.com>
2021-07-28 18:42:09 +0200

    Make shiny app less reactive by only updating on strict changes to relevant inputs / triggers (using shiny::observeEvent()) instead of any change (using shiny::observe())

--- R/app_server.R
+++ R/app_server.R
@@ -13,11 +13,11 @@ local <- FALSE # set to FALSE when deploying, TRUE when testing locally
 # get truth data:
 if (local) {
   dat_truth <- read.csv("../../viz/truth_to_plot.csv",
-    colClasses = list("date" = "Date"), stringsAsFactors = FALSE
+                        colClasses = list("date" = "Date"), stringsAsFactors = FALSE
   )
 } else {
   dat_truth <- read.csv("https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/viz/truth_to_plot.csv",
-    colClasses = list("date" = "Date"), stringsAsFactors = FALSE
+                        colClasses = list("date" = "Date"), stringsAsFactors = FALSE
   )
 }
 # adapt column names for matching with targets
@@ -27,82 +27,63 @@ colnames(dat_truth) <- gsub("inc_", "inc ", colnames(dat_truth))
 cols_legend <- c("#699DAF", "#D3D3D3")
 
 app_server <- function(input, output, session) {
-  dat <- reactiveValues()
 
-  # Handle reading in of files:
-  observe({
-    inFile <- input$file # file upload
+  model <- reactiveVal("")
+  forecasts <- reactiveVal(NULL)
+  locations <- reactiveVal(NULL)
+
+  # if URL is provided as a URL param
+  observeEvent(session$clientData, {
     query <- parseQueryString(session$clientData$url_search) # arguments provided in URL
-
-    # initialization:
-    dat$path <- ""
-    dat$forecasts <- NULL
-
-    # if path to csv provided in URL:
-    if (!is.null(query$file) & is.null(inFile) & input$path == "") {
-      dat$path <- query$file
-      dat$name <- basename(query$file)
-      dat$forecasts <- NULL
-      try(dat$forecasts <- read_week_ahead(dat$path)) # wrapped in try() to avoid crash if no valid csv
+    if (length(query) != 0) {
+      model(basename(query$file))
+      forecasts(try(read_week_ahead(query$file)))
     }
-
-    # if file uploaded:
-    if (!is.null(inFile) & input$path == "") {
-      dat$path <- inFile$datapath
-      dat$name <- basename(inFile$name)
-      dat$forecasts <- NULL
-      try(dat$forecasts <- read_week_ahead(dat$path)) # wrapped in try() to avoid crash if no valid csv
-    }
-
-    # if path to csv provided in input field:
-    if (input$path != "") {
-      dat$path <- input$path
-      dat$name <- basename(input$path)
-      dat$forecasts <- NULL
-      try(dat$forecasts <- read_week_ahead(dat$path)) # wrapped in try() to avoid crash if no valid csv
-    }
-
-    # extract locations:
-    if (!is.null(dat$forecasts)) {
-      locations <- unique(dat$forecasts$location)
-      if (!is.null(dat$forecasts$location_name)) {
-        names(locations) <- unique(dat$forecasts$location_name)
-      }
-      dat$locations <- locations
-    }
-
-    print(dat$path)
   })
 
-  # input element to select location (loads the available locations):
-  # (currently not used)
-  # output$inp_select_location <- renderUI(
-  #   selectInput("select_location", "Select location:", choices = dat$locations,
-  #               selected = "GM")
-  # )
+  # if data file is uploaded via the file explorer:
+  observeEvent(input$file, {
+    path <- input$file$datapath
+    model(basename(input$file$name))
+    forecasts(try(read_week_ahead(path))) # wrapped in try() to avoid crash if no valid csv
+  }, ignoreInit = TRUE)
+
+  # if URL to csv provided in input field:
+  observeEvent(input$path, {
+    path <- input$path
+    model(basename(input$path))
+    forecasts(try(read_week_ahead(path))) # wrapped in try() to avoid crash if no valid csv
+  }, ignoreInit = TRUE)
+
+  observeEvent(forecasts(), {
+    locations(unique(forecasts()$location))
+    if (!is.null(forecasts()$location_name)) {
+      names(locations()) <- unique(forecasts()$location_name)
+    }
+  })
 
   # output element to display file name:
-  output$file_name <- renderText(dat$name)
+  output$file_name <- renderText(model())
 
   # plot output:
   output$plot <- renderPlot({
-    if (!is.null(dat$forecasts)) {
+    if (!is.null(forecasts())) {
 
       # get forecast date:
-      forecast_date <- dat$forecasts$forecast_date[1]
+      forecast_date <- forecasts()$forecast_date[1]
 
-      par(mfrow = c(length(dat$locations), 2), cex = 1)
+      par(mfrow = c(length(locations()), 2), cex = 1)
 
-      for (loc in dat$locations) {
+      for (loc in locations()) {
         # plot for cases:
-        if (any(grepl("case", dat$forecasts$target))) { # only if case forecasts available
-          plot_forecast(dat$forecasts,
-            forecast_date = forecast_date,
-            location = loc,
-            truth = dat_truth, target_type = "inc case",
-            levels_coverage = c(0.5, 0.95),
-            start = as.Date(forecast_date) - 35,
-            end = as.Date(forecast_date) + 28
+        if (any(grepl("case", forecasts()$target))) { # only if case forecasts available
+          plot_forecast(forecasts(),
+                        forecast_date = forecast_date,
+                        location = loc,
+                        truth = dat_truth, target_type = "inc case",
+                        levels_coverage = c(0.5, 0.95),
+                        start = as.Date(forecast_date) - 35,
+                        end = as.Date(forecast_date) + 28
           )
           title(paste0("Incident cases - ", loc))
           legend("topleft", legend = c("50%PI", "95% PI"), col = cols_legend, pch = 15, bty = "n")
@@ -112,14 +93,14 @@ app_server <- function(input, output, session) {
         }
 
         # plot for deaths:
-        if (any(grepl("death", dat$forecasts$target))) { # only if case forecasts available
-          plot_forecast(dat$forecasts,
-            forecast_date = forecast_date,
-            location = loc,
-            truth = dat_truth, target_type = "inc death",
-            levels_coverage = c(0.5, 0.95),
-            start = as.Date(forecast_date) - 37,
-            end = as.Date(forecast_date) + 28
+        if (any(grepl("death", forecasts()$target))) { # only if case forecasts available
+          plot_forecast(forecasts(),
+                        forecast_date = forecast_date,
+                        location = loc,
+                        truth = dat_truth, target_type = "inc death",
+                        levels_coverage = c(0.5, 0.95),
+                        start = as.Date(forecast_date) - 37,
+                        end = as.Date(forecast_date) + 28
           )
           title(paste0("Incident deaths - ", loc))
           legend("topleft", legend = c("50%PI", "95% PI"), col = cols_legend, pch = 15, bty = "n")
@@ -136,6 +117,6 @@ app_server <- function(input, output, session) {
   })
 
   output$plot_ui <- renderUI({
-    plotOutput("plot", height = ifelse(is.null(dat$locations), 500, length(dat$locations) * 250))
+    plotOutput("plot", height = ifelse(is.null(locations()), 500, length(locations()) * 250))
   })
 }
