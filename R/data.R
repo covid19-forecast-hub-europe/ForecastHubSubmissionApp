be884e21edef5b689fcbecad11a5a3e51cc76fdc
Hugo Gruson <hugo.gruson@protonmail.com>
2021-10-29 12:45:47 +0200

    Document data set

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Truth data set containing observed cases
+#'
+#' This truth data set contains observed cases of Covid-19 in countries covered
+#' by the European Covid-19 Forecast Hub.
+#'
+#' @source Automatically generated and updated by <https://github.com/epiforecasts/covid19-forecast-hub-europe/blob/main/viz/prepare_truth_data.R>
+"truth"
